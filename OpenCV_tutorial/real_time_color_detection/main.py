fa34db26ca935531c5366e157aaacb15fc509769
Shylesh S <shylesh1640@gmail.com>
2026-03-15 19:53:09 +0530

    real time color dector

---
+++ OpenCV_tutorial/real_time_color_detection/main.py
@@ -0,0 +1,25 @@
+import cv2
+from PIL import Image
+from util import get_limits
+
+color = (255, 0, 0)  # Blue color in BGR
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_limit, upper_limit = get_limits(color)
+    mask = cv2.inRange(hsv, lower_limit, upper_limit)
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    print(bbox)
+    cv2.imshow('Frame', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
